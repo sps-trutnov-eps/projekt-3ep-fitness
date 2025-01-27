e2e5d257222d0efe7a5bb5da3030f45eb5dcae40
Matyáš Sýs <sysm22@spstrutnov.cz>
2025-01-27 21:04:12 +0100

    Initial setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routers/indexRouter');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+// Server static files
+app.use(express.static('www'));
+
+// Initialize routes
+app.use('/', indexRouter);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
